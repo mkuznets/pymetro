24ccfa6e89359428815725749870d15da3cd7974
dkbrz <glaz.dikobraz@gmail.com>
2018-06-27 13:55:29 +0300

    fixed

--- pymetro/router.py
+++ pymetro/router.py
@@ -1,42 +1,35 @@
 # coding=utf-8
 
 from .types import Route, Station
-
 from heapdict import heapdict
 from pymetro.types import Route, Station
 from data import STATIONS, LINKS, LINES
 
-from collections import defaultdict, deque
+from collections import defaultdict
 
 class Router:
 
     def __init__(self):
-        self.graph = defaultdict(list)
+        self.graph = defaultdict(heapdict)
         for s1, s2, time in LINKS:
-            self.graph[Station(s1)].append(Route([Station(s1),Station(s2)], time))
-            self.graph[Station(s2)].append(Route([Station(s2),Station(s1)], time))
-        self.time = 0
-        self.path = []
+            self.graph[Station(s1)][Station(s2)]=time
+            self.graph[Station(s2)][Station(s1)]=time
 
     def make_route(self, source: Station, target: Station) -> Route:
+        queue = heapdict({key:float('inf') for key in self.graph})
         dijkstra = {key:float('inf') for key in self.graph}
+        queue[source] = 0
         dijkstra[source] = 0
-        queue = deque([source])
-        seen = set([source])
         paths = {source:[source]}
         while queue:
-            node = queue.popleft()
-            seen.update([node])
+            node, k = queue.popitem()
             if node in self.graph:
-                for route in self.graph[node]:
-                    if route.path[0] == node: s, t = route.path[0], route.path[1]
-                    else: s, t = route.path[1], route.path[0]
-                    if t not in seen:
-                        new = dijkstra[node] + route.time
+                for neighbor in self.graph[node]:
+                    t = neighbor
+                    if t in queue:
+                        new = k + self.graph[node][neighbor]
                         if new < dijkstra[t]:
+                            if t in queue: queue[t] = new
                             dijkstra[t] = new
                             paths[t] = paths[node]+[t]
-                        queue.append(t)
-        self.time = dijkstra[target]
-        self.path = paths[target]
-        return self
+        return Route(paths[target], dijkstra[target])
